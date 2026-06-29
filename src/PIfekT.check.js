1aa0bdec7702f6cabb1da827ecfca97396dd9efb
iki1uc <entertrainer@web.de>
2026-06-29 17:53:30 +0200

    Update PIfekT.check.js

--- src/PIfekT.check.js
+++ src/PIfekT.check.js
@@ -1,1 +1,26 @@
+// PIfekT.check.js
+// RAW-Check-Modul für Validierungen
+
+export class PIfekTCheck {
+  static isValidItem(obj) {
+    return (
+      obj &&
+      typeof obj.id !== 'undefined' &&
+      typeof obj.label === 'string' &&
+      typeof obj.type === 'string'
+    );
+  }
+
+  static hasValue(obj) {
+    return obj && obj.value !== null && obj.value !== undefined;
+  }
+
+  static summary(obj) {
+    return {
+      id: obj.id,
+      ok: this.isValidItem(obj),
+      hasValue: this.hasValue(obj),
+    };
+  }
+}
 
