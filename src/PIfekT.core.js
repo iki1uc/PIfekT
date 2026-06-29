43d3a4e5012d5bcb723479f9349da63c7d6f211b
iki1uc <entertrainer@web.de>
2026-06-29 17:53:01 +0200

    Add PIfekTCore class with item management methods
    Implement core logic for PIfekT with methods to add, get, remove items and convert to object.

--- src/PIfekT.core.js
+++ src/PIfekT.core.js
@@ -1,1 +1,37 @@
+// PIfekT.core.js
+// RAW-Kernlogik für PIfekT
+
+import { PIfekTItem } from './PIfekT.item.js';
+
+export class PIfekTCore {
+  constructor() {
+    this.items = new Map();
+  }
+
+  add(item) {
+    if (!(item instanceof PIfekTItem)) return false;
+    this.items.set(item.id, item);
+    return true;
+  }
+
+  get(id) {
+    return this.items.get(id) || null;
+  }
+
+  all() {
+    return [...this.items.values()];
+  }
+
+  remove(id) {
+    return this.items.delete(id);
+  }
+
+  toObject() {
+    return this.all().map(i => i.toObject());
+  }
+}
+
+export function createCore() {
+  return new PIfekTCore();
+}
 
